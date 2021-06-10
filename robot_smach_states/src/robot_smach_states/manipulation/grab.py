63ed96cd7c02329eaadd4552a4fe9fa57150fb6d
JeroenBerghs <j.c.a.berghs@student.tue.nl>
2021-06-10 12:01:15 +0200

    Changed start_recording()

--- robot_smach_states/src/robot_smach_states/manipulation/grab.py
+++ robot_smach_states/src/robot_smach_states/manipulation/grab.py
@@ -213,7 +213,7 @@ class PickUp(smach.State):
         if not arm.send_goal(goal_bl, timeout=20, allowed_touch_objects=[grab_entity.id]):
             rospy.logerr('Failed lift')
 
-        self.robot.grasp_detector.start_recording() # start recording torque data for object detection
+        self.robot.arm.grasp_detector.start_recording() # start recording torque data for object detection
 
         # Retract
         goal_bl = grasp_framestamped.projectToFrame(self.robot.base_link_frame,
